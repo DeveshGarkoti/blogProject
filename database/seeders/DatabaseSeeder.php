af2f294cfdc588bba36a592e59d24c45ca25768a
DeveshGarkoti <deveshgar280@gmail.com>
2024-09-11 14:03:46 -0700

    basic table and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Post::factory(100)->create();
+        Category::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
